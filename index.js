3c32236c1ffe0bbc068e6f12779ba253a4dbb4db
JadenVidal <jaden.vidal@sky.uk>
2019-03-21 11:22:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 /* Your code goes here */
+var customerName = 'bob';
+const leastFavoriteCustomer = 'jaden';
+
+const upperCaseCustomerName = () =>{
+ customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+const overWriteBestCustomer = () =>{
+    bestCustomer = 'maybe bob';
+}
+
+const changeLeastFavoriteCustomer = () =>{
+    leastFavoriteCustomer = 'not jaden';
+}
+
+const attemptTwoFavoriteCustomers = () =>{
+    let favoriteCustomer = 'favoriteCustomer';
+    let favoriteCustomer = 'jaden';
+}
